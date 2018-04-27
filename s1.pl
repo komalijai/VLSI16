d139689ab030a1a513c8498911d08087bfafb3a6
komalijai <jaideep.komali@gmail.com>
2018-04-27 09:45:47 +0530

    This is the second updated file in the system.

---
+++ s1.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/perl
+#$j="my name is jaideep";
+#$j =~ s/$j/my name is seenu/;
+#$j =~ s/jaideep/rk/;
+#print "$j\n";
+
+
+$m="my mail ID is jsubba\@gmail.com";
+print "$m\n";
+if ($m=~/(\w+)(\@\w+\.com)/)
+	{
+	$m=~s/(\w+)(\@\w+\.com)/komali$2/;
+	print "$m\n";
+
+	}
+
+
+$s1="my mail ID is $ARGV[0]";
+$ARGV[1]=~s/(\@\w+\.com)//;
+print "$s1\n";
+if($s1=~/(\w+)(\@\w+\.com)/)
+	{
+	$s1=~s/(\w+)(\@\w+\.com)/$ARGV[1]${2}/;
+	print"$s1\n";
+	}
