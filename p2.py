1c0dd22fa19455283ca42c51ca40dbb7bcb85953
komalijai <jaideep.komali@gmail.com>
2018-04-27 09:34:32 +0530

    This is Latest File

--- p2.py
+++ p2.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python
 a =10 ;
-if a== 10:
+if a== 330:
 	print("The value is 10");
 elif a!=10:
-	print("number is not 10");
+	print("number is not 20");
 else:
 	print("not a number");
 
